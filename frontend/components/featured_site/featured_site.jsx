5bbcdaf57b89151fec297a4dc1b778cdf97b7d91
Dallas Morgan <dallaswmorgan@gmail.com>
2017-01-13 16:17:28 -0800

    Image displaying on home page

--- frontend/components/featured_site/featured_site.jsx
+++ frontend/components/featured_site/featured_site.jsx
@@ -4,20 +4,30 @@ class FeaturedSite extends React.Component {
   constructor(props) {
     super(props);
   }
-
-  componentDidMount() {
+  componentWillMount() {
     this.props.fetchFeaturedSites();
   }
 
   render() {
-    if (this.props.sites) {
-      return(
-        <div>{this.props.sites.map(site => (
-            <h1>{site.name}</h1>
-          ))}</div>
-      );
-    }
-    return( <div></div>);
+    const currentSite = this.props.sites[0];
+    if (currentSite) {
+    return(
+      <div className="featured-sites-box">
+        <div className="featured-site-text">
+          <h3>Find your happy place at camps like</h3>
+          <br/>
+          <h1>{currentSite.name}</h1>
+        </div>
+        <br/>
+        <div className="featured-site-pic" key={currentSite.id}>
+          <img
+            src="http://res.cloudinary.com/dallaswmorgan/image/upload/v1484267441/russian-river-stock_q6wou3.jpg"/>
+        </div>
+      </div>
+    );
+  } else {
+    return(<div></div>);
+  }
   }
 }
 
--- frontend/components/featured_site/featured_site_container.jsx
+++ frontend/components/featured_site/featured_site_container.jsx
@@ -4,7 +4,6 @@ import {fetchFeaturedSites} from '../../actions/site_actions';
 
 const mapStateToProps = state => ({
   sites: Object.keys(state.sites).map( id => (state.sites[id]))
-
 });
 
 const mapDispatchToProps = dispatch => ({
